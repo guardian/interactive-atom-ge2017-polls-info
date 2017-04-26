657c22a5787d92a94d3cfb222ae379ef21549de8
blongden73 <hello@benlongdendesign.co.uk>
2017-04-26 11:03:17 +0100

    settng up polls info boxes

--- src/render.js
+++ src/render.js
@@ -1,5 +1,14 @@
 import mainTemplate from './src/templates/main.html!text'
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    return mainTemplate;
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/10-vIpZE0YzDUboflfHkI6xZBsAS3U7PwZacbo93aA2E.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
 }
